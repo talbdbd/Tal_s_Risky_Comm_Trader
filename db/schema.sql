f7820b2797bbb0fd252b9e7b0d1731481d96377e
talbdbd <bendavid.bd.tal@gmail.com>
2024-07-01 15:50:48 -0700

    added .env file and modified models directory

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,32 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+USE ecommerce_db;
+
+-- CREATE TABLES
+CREATE TABLE category (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE product (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(30) NOT NULL,
+    price DECIMAL NOT NULL,
+    stock INT NOT NULL,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+CREATE TABLE tag (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE product_tag (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT,
+    tag_id INT,
+    FOREIGN KEY (product_id) REFERENCES product(id),
+    FOREIGN KEY (tag_id) REFERENCES tag(id)
+);
